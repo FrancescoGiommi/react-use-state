20400e7e5ce5b55c9441141fcea9674b80295145
FrancescoGiommi <francescogiommi97@icloud.com>
2025-01-30 18:56:18 +0100

    Add fix

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,26 +4,32 @@ import Card from "../components/Card";
 
 /* Export Main */
 export default function Main() {
-  const activeLanguage = languages[0];
-  const [activePost, setActivePost] = useState(activeLanguage);
+  const isSelected = true;
+  const activePost = languages[0];
+
+  const [activeLanguage, setActiveLanguage] = useState(activePost);
 
   const handleSelectTag = (item) => {
-    const newPost = languages.find((post) => post.title === item);
-    console.log(newPost);
+    const newLanguage = languages.find((language) => language.title === item);
+    return setActiveLanguage(newLanguage);
   };
   return (
     <>
       <main>
         {/* Generate card with Map */}
-        {languages.map((item) => (
+        {languages.map((language) => (
           <ul className="d-inline-flex gap-2 ">
             <li>
               <button
-                className="btn btn-primary "
-                key={item.id}
-                onClick={() => handleSelectTag()}
+                className={`btn ${
+                  activeLanguage.title === language.title
+                    ? "btn-warning"
+                    : "btn-primary"
+                }`}
+                key={language.id}
+                onClick={() => handleSelectTag(language.title)}
               >
-                {item.title}
+                {language.title}
               </button>
             </li>
           </ul>
@@ -33,9 +39,9 @@ export default function Main() {
 
         <div>
           <Card
-            key={activePost.id}
-            title={activePost.title}
-            description={activePost.description}
+            key={activeLanguage.id}
+            title={activeLanguage.title}
+            description={activeLanguage.description}
           />
         </div>
       </main>
